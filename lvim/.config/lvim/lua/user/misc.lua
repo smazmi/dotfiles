9704a5f365de4bd48ebcd987ec95d8ad29a28655
smazmi <sadiquemobaraka5@gmail.com>
2024-04-04 04:38:27 +0530

    Fix: notify-nvim background color issue

---
+++ lvim/.config/lvim/lua/user/misc.lua
@@ -0,0 +1,14 @@
+local cmp_nvim_lsp = require "cmp_nvim_lsp"
+
+require("lspconfig").clangd.setup {
+  on_attach = on_attach,
+  capabilities = cmp_nvim_lsp.default_capabilities(),
+  cmd = {
+    "clangd",
+    "--offset-encoding=utf-16",
+  },
+}
+
+require('notify').setup({
+  background_colour = "#000000"
+})
