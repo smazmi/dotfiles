8c7c41448443c7899b948cb2ff0b196d044b7881
smazmi <sadiquemobaraka5@gmail.com>
2024-04-04 04:38:46 +0530

    Add misc.lua

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -8,16 +8,4 @@ reload('user.lsp')
 reload('user.dashboard')
 -- reload('user.lsp-status')
 reload('user.autocmds')
-
-
-
-local cmp_nvim_lsp = require "cmp_nvim_lsp"
-
-require("lspconfig").clangd.setup {
-  on_attach = on_attach,
-  capabilities = cmp_nvim_lsp.default_capabilities(),
-  cmd = {
-    "clangd",
-    "--offset-encoding=utf-16",
-  },
-}
+reload('user.misc')
