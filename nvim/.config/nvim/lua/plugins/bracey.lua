18ea4482b607dab46134ee1dc19e16a45d44fefa
smazmi <sadiquemobaraka5@gmail.com>
2025-04-17 20:46:14 +0530

    chore: cleanup

--- nvim/.config/nvim/lua/plugins/bracey.lua
+++
@@ -1,5 +0,0 @@
-return {
-    "turbio/bracey.vim",
-    cmd = { "Bracey", "BracyStop", "BraceyReload", "BraceyEval" }, -- Live Server for html, css and js.
-    build = "npm install --prefix server",
-  }
