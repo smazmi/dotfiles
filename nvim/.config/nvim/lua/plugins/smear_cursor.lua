2a134d8306c701cd218b21db7d3f1b28f8064696
Sadique Azmi <sadiquemobaraka5@gmail.com>
2026-01-04 05:09:51 +0530

    add(nvim): smear-cursor opts
    Signed-off-by: Sadique Azmi <sadiquemobaraka5@gmail.com>

--- nvim/.config/nvim/lua/plugins/smear_cursor.lua
+++ nvim/.config/nvim/lua/plugins/smear_cursor.lua
@@ -1,4 +1,9 @@
 return {
   "sphamba/smear-cursor.nvim",
-  opts = {},
+  opts = {
+    legacy_computing_symbols_support = true,
+    stiffness = 0.5,
+    trailing_stiffness = 0.5,
+    matrix_pixel_threshold = 0.5,
+  },
 }
