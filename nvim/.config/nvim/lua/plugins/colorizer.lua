9ae591882ef7dceb294987fd6d9c8ec23d55de8c
Sadique Azmi <sadiquemobaraka5@gmail.com>
2026-05-23 22:09:58 +0530

    fix(nvim): add colorizer and indent rainbowline
    Signed-off-by: Sadique Azmi <sadiquemobaraka5@gmail.com>

--- nvim/.config/nvim/lua/plugins/colorizer.lua
+++ nvim/.config/nvim/lua/plugins/colorizer.lua
@@ -1,9 +1,3 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  opts = {
-    flavour = "mocha",
-    transparent_background = true,
-    -- configuration options...
-  },
+  "norcalli/nvim-colorizer.lua",
 }
